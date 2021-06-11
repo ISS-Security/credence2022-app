9b727a7efe48d132c44264d02d57d503b75cb768
Soumya Ray <soumya.ray@gmail.com>
2021-06-11 17:56:18 +0800

    Handles SSO login via OAuth

--- spec/integration/service_authenticate_spec.rb
+++ spec/integration/service_authenticate_spec.rb
@@ -31,7 +31,7 @@ describe 'Test Service Objects' do
              .to_return(body: auth_return_json,
                         headers: { 'content-type' => 'application/json' })
 
-      auth = Credence::AuthenticateAccount.new(app.config).call(**@credentials)
+      auth = Credence::AuthenticateAccount.new.call(**@credentials)
 
       account = auth[:account]['attributes']
       _(account).wont_be_nil
@@ -42,10 +42,10 @@ describe 'Test Service Objects' do
     it 'BAD: should not find a false authenticated account' do
       WebMock.stub_request(:post, "#{API_URL}/auth/authenticate")
              .with(body: @mal_credentials.to_json)
-             .to_return(status: 403)
+             .to_return(status: 401)
       _(proc {
-        Credence::AuthenticateAccount.new(app.config).call(**@mal_credentials)
-      }).must_raise Credence::AuthenticateAccount::UnauthorizedError
+        Credence::AuthenticateAccount.new.call(**@mal_credentials)
+      }).must_raise Credence::AuthenticateAccount::NotAuthenticatedError
     end
   end
 end
